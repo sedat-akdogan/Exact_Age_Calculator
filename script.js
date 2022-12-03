1c95218ef7ce6e74314b795bfd611934b5c90a69
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-03 01:39:51 +0000

    birthday image added

--- script.js
+++ script.js
@@ -35,10 +35,41 @@ window.addEventListener("load", () => {
     });
 });
 
+let selectedBirthday;
 let birthdayInput = document.querySelector("input[name=birthday]");
 birthdayInput.addEventListener("change", (event) => {
-    let selectedBirthday = new Date(event.target.value);
+    // event.target == birthdayInput
+    // convert to datee from dateString
+    selectedBirthday = new Date(event.target.value);
     if (selectedBirthday > new Date()) {
         alert("Selected birthday is invalid!");
+        return;
     }
+
+    document.body.style.backgroundImage = "ur1('https://images.unsplash.com/photo-1467810563316-b5476525c0f9?ixlib=rb-1.2.18ixid-eyJhcHBfawQiojEyMDd9&auto=format&fit=crop&W=13498g-80')";
+
+    loading.style.display = "block";
+    setInterval(updateCountdown, 1000);
+    setTimeout(() => {
+        loading.style.display = "none";
+    }, 1000);
 });
+
+const updateCountdown = () => {
+    let dobYear = selectedBirthday.getFullYear();
+    let dobMonth = selectedBirthday. getMonth();
+    let dobDay = selectedBirthday. getDate();
+    let dobHours = selectedBirthday. getHours();
+    let dobMinutes = selectedBirthday.getMinutes();
+    let dobseconds = selectedBirthday. getSeconds();
+
+    let now = new Date();
+
+    let currentYear = selectedBirthday. getFullYear();
+    let currentMonth = selectedBirthday. getMonth();
+    let currentDay = selectedBirthday.getDate();
+    let currentHours = selectedBirthday. getHours();
+    let currentMinutes = selectedBirthday.getMinutes();
+    let currentSeconds = selectedBirthday.getSeconds();
+    
+}
