474b8113b72fb3892c85600945b309933dbb75e0
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-03 20:51:34 +0000

    sec, min, hour onder has been changed

--- script.js
+++ script.js
@@ -15,43 +15,38 @@ window.addEventListener("load", () => {
     countdown.style.display = "flex";
   }, 1000);
 
-  // years.innerHTML = "00";
-  // months.innerHTML = "00";
-  // days.innerHTML = "00";
-  // hours.innerHTML = "00";
-  // minutes.innerHTML = "00";
-  // seconds.innerHTML = "00";
+  years.innerHTML = "00";
+  months.innerHTML = "00";
+  days.innerHTML = "00";
+  hours.innerHTML = "00";
+  minutes.innerHTML = "00";
+  seconds.innerHTML = "00";
 
   // let H2Elements = document.getElementsByTagName("h2");
   // for (let index = 0; index < H2Elements.length; index++) {
   //     H2Elements[index].innerHTML = "00";
   // }
 
-  let H2Elements2 = countdown.querySelectorAll("h2");
-  H2Elements2.forEach((element) => {
-    element.innerHTML = "00";
-  });
-
+  // let H2Elements2 = countdown.querySelectorAll("h2");
+  // H2Elements2.forEach((element) => {
+  //   element.innerHTML = "00";
+  // });
 });
 
 let selectedBirthday;
-//let birthdayInput = document.querySelector("input[name=birthday]");
-let birthdayInput = document.getElementById("birthday");
+let birthdayInput = document.querySelector("input[name=birthday]");
+// let birthdayInput = document.getElementById("birthday");
 birthdayInput.addEventListener("change", (event) => {
   selectedBirthday = new Date(event.target.value);
-  if(selectedBirthday > new Date()) {
+  if (selectedBirthday > new Date()) {
     alert("Selected birthday is invalid!");
     return;
   }
 
-  document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1467810563316-b5476525c0f9?ixlib=rb-1.2.18ixid-eyJhcHBfawQiojEyMDd9&auto=format&fit=crop&W=13498g-80')";
+  document.body.style.backgroundImage ="url('https://images.unsplash.com/photo-1467810563316-b5476525c0f9?ixlib=rb-1.2.18ixid-eyJhcHBfawQiojEyMDd9&auto=format&fit=crop&W=13498g-80')";
 
-  loading.style.display = "block";
   setInterval(updateCountdown, 1000);
-  setTimeout(() => {
-    loading.style.display = "none";
-  }, 1000);
-
+  
 });
 
 const updateCountdown = () => {
@@ -78,32 +73,31 @@ const updateCountdown = () => {
   let minuteOfAge = currentMinute - dobMinute;
   let secondOfAge = currentSecond - dobSecond;
 
-  // monthofAge control
-  if (monthOfAge < 0) {
-    monthOfAge += 12;
-    yearOfAge--;
-  }
-  // dayofage control
-  if (dayOfAge < 0) {
-    dayOfAge += 30;
-    monthOfAge--;
-  }
-  // hourofAge control
-  if (hourOfAge < 0) {
-    hourOfAge += 24;
-    dayOfAge--;
+  // secondofAge control
+  if (secondOfAge < 0) {
+    secondOfAge += 60;
+    minuteOfAge--;
   }
   //minuteofAge control
   if (minuteOfAge < 0) {
     minuteOfAge += 60;
     hourOfAge--;
   }
-  // secondofAge control
-  if (secondOfAge < 0) {
-    secondOfAge += 60;
-    minuteOfAge--;
-    }
-
+  // hourofAge control
+  if (hourOfAge < 0) {
+    hourOfAge += 24;
+    dayOfAge--;
+  }
+  // dayofage control
+  if (dayOfAge < 0) {
+    dayOfAge += 30;
+    monthOfAge--;
+  }
+  // monthofAge control
+  if (monthOfAge < 0) {
+    monthOfAge += 12;
+    yearOfAge--;
+  }
   // add values to DOM
   years.innerHTML = yearOfAge.toString().padStart(2, "0");
   months.innerTML = monthOfAge.toString().padStart(2, "0");
